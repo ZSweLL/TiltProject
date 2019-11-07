e8d8cceaf36b3803c13c055cf784245e78cb6d52
Student <student@DM013.local>
2019-11-07 10:22:56 -0600

    Added additional levels and jump items

---
+++ Assets/ForcePlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePlatform : MonoBehaviour
+{
+
+    public float forceSpeed = 20;
+    public bool destroyAfterUse = false;
+
+    Vector3 dir;
+
+    void Start () {
+        dir = this.transform.up;
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            if(other.gameObject.GetComponent<Rigidbody>() != null) {
+                other.gameObject.GetComponent<Rigidbody>().AddForce(dir * forceSpeed, ForceMode.Impulse);
+
+                if(destroyAfterUse) {
+                    Destroy(this.gameObject, 0.25f);
+                }
+            }
+        }
+    }
+}
--- Assets/MarbleMovement.cs
+++ Assets/MarbleMovement.cs
@@ -6,6 +6,8 @@ public class MarbleMovement : MonoBehaviour
 {
 
     Vector3 dir;
+
+    Vector3 calibratedDir;
     Rigidbody rb;
 
     public bool debug = true;
@@ -16,6 +18,44 @@ public class MarbleMovement : MonoBehaviour
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        Calibrate();
+        startPos = this.transform.position;
+    }
+
+    public void Calibrate() {
+        calibratedDir.x = Input.acceleration.x;
+        calibratedDir.z = Input.acceleration.y;
+        Debug.Log("Calibrated Dir = " + calibratedDir);
+    }
+
+    public float jumpSpeed = 5f;
+    public void Jump() {
+        if(isGrounded) {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    bool isGrounded = true;
+
+    void OnCollisionEnter() {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit() {
+        isGrounded = false;
+    }
+
+    public int score = 0;
+    bool canJump = false;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name == "JumpPowerUp") {
+            canJump = true;
+        }
+        if(other.gameObject.CompareTag("Coin")) {
+            score += 250;
+            Destroy(other.gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -23,8 +63,8 @@ public class MarbleMovement : MonoBehaviour
     {
         //Vector3 dir = Vector3.zero; // new Vector3(0,0,0);
 
-        dir.x = Input.acceleration.x;
-        dir.z = Input.acceleration.y;
+        dir.x = Input.acceleration.x - calibratedDir.x;
+        dir.z = Input.acceleration.y - calibratedDir.y;
         if(debug) {
             Debug.DrawRay(this.transform.position, dir * 2, Color.red, 0.5f);
         }
@@ -36,6 +76,17 @@ public class MarbleMovement : MonoBehaviour
             Vector3 scale = Vector3.one;
             scale.z = dir.magnitude * 2;
             arrowIndicator.localScale = scale;
+
+            if(this.transform.position.y <= 0) {
+                ResetPosition();
+            }
+        }
+
+        Vector3 startPos;
+
+        void ResetPosition() {
+            this.transform.position = startPos;
+            rb.velocity = Vector3.zero;  // this stops all movement.
         }
 
     void FixedUpdate() {
